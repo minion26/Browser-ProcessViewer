6d42f5daa0a6550fae9575e0336f902328715f56
Jitca Diana <jitcadiana6@gmail.com>
2023-12-11 12:47:02 +0200

    implemented the run command

--- process.py
+++ process.py
@@ -83,6 +83,26 @@ def kill_proces(pid):
         print(f"The process with the PID {pid} is killed.")
 
 
+def start_proces(path, params=None):
+    # start a process with the given path and params
+    try:
+        command = [path]
+        if params is not None:
+            command.extend(params.split())
+
+        if platform.system() == 'Windows':
+            # On Windows, use CREATE_NEW_PROCESS_GROUP to detach the process
+            proc = sp.Popen(command, creationflags=sp.CREATE_NEW_PROCESS_GROUP)
+        else:
+            proc = sp.Popen(command, preexec_fn=os.setpgrp)  # On Unix-like systems
+
+    except FileNotFoundError:
+        raise ValueError(f"Executable {path} not found.")
+    else:
+        print(f"Process with PID {proc.pid} has been started.")
+        return proc.pid
+
+
 if __name__ == "__main__":
     command = sys.argv[1]
     if command == "help":
@@ -102,3 +122,11 @@ if __name__ == "__main__":
     elif command == "kill":
         pid = int(sys.argv[2])
         kill_proces(pid)
+    elif command == "run":
+        path = sys.argv[2]
+        # if params is empty, it will be None
+        if len(sys.argv) == 3:
+            params = None
+        else:
+            params = sys.argv[3]
+        start_proces(path, params)
